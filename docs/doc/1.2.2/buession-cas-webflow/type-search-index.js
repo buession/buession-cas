95b99df86c3349a4ede1f2299a7795dcba3d8795
Yong.Teng <webmaster@buession.com>
2021-07-07 23:59:28 +0800

    add javadoc for 1.2.2

---
+++ docs/doc/1.2.2/buession-cas-webflow/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"CasCaptchaConfiguration","h":"","u":""},{"l":"CasCaptchaWebflowConfigurer","h":"","u":""},{"l":"InitializeCaptchaAction","h":"","u":""},{"l":"ValidateCaptchaAction","h":"","u":""},{"l":"所有类","h":"","u":"allclasses-index.html"}]
