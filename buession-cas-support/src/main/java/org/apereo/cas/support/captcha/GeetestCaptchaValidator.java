58b0bfb22d8acdd5ded1a502292f8dd8ae3e926a
Yong.Teng <webmaster@buession.com>
2022-04-28 00:05:59 +0800

    版本升级至 2.0.0

--- buession-cas-support/src/main/java/org/apereo/cas/support/captcha/GeetestCaptchaValidator.java
+++ buession-cas-support/src/main/java/org/apereo/cas/support/captcha/GeetestCaptchaValidator.java
@@ -21,10 +21,19 @@
  * | Author: Yong.Teng <webmaster@buession.com> 													       |
  * | Copyright @ 2013-2022 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
- */package org.apereo.cas.support.captcha;/**
- * 
- *
+ */
+package org.apereo.cas.support.captcha;
+
+import com.buession.lang.Status;
+
+/**
  * @author Yong.Teng
  * @since 2.0.0
- */public class GeetestCaptchaValidator {
+ */
+public class GeetestCaptchaValidator implements CaptchaValidator {
+
+	@Override
+	public Status validate(String code, String userAgent){
+		return null;
+	}
 }
