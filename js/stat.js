65d5f9fbcc3c14de97903fc44a17f66e62462443
eduosi <eduosi@users.noreply.github.com>
2026-09-08 16:37:25 +0800

    【部署】：2026-09-08 16:37:25

---
+++ js/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c3858b3150a01c29a43a736189b3aaf9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
